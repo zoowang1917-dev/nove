89b14018d526a88a7c6dd0d6a75de4b8c786b1e9
zoowang1917-dev <zoowang1917@gmail.com>
2026-03-22 17:58:13 +0800

    更新 database.dart

--- lib/core/db/database.dart
+++ lib/core/db/database.dart
@@ -418,7 +418,7 @@ class AppDatabase {
   Future<void> rollbackToSnapshot(String bookId, int chapterNo) async {
     final snap = await getSnapshot(bookId, chapterNo);
     for (final e in snap.entries) {
-      await insertArchiveSnapshot(bookId, e.key, e.value, chapterNo);
+      await insertArchiveSnapshot(bookId: bookId, type: e.key, content: e.value, chapterNo: chapterNo);
     }
     // 删除该快照点之后的快照
     await (await database).delete('archive_snapshots',
